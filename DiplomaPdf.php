383ef06d3d6a9746f2e4fa1efb61996fe3fa4977
cidescorpo <51003050+cidescorpo@users.noreply.github.com>
2019-07-10 16:13:00 -0400

    Add files via upload

--- DiplomaPdf.php
+++ DiplomaPdf.php
@@ -8,7 +8,7 @@ public function __construct($post,$unit,$type)
 parent::__construct($post,$unit,$type);
 $this->AddPage();
 $this->SetFont('Arial','B',16);
-$this->Image('DiplomaFirna_Duran_1907.jpg',0,0,280);
+$this->Image('Diploma_Tipo_1901.jpg',0,0,280);
 
 
 }
@@ -21,12 +21,44 @@ public function renderFirma($firma, $x , $y ,$res='')
 
 public function nombreCurso($nombre)
 {
- 
+
+	
+    $nombre_diploma = "nombre diploma 1";
+	$nombre_diploma2 = "nombre diploma 2";
  	$this->SetFont('Arial','',35);
 	$this->Ln(70);
     $this->Cell(0,10, $nombre, 0, 1, 'C');
-}
+	
+	
+	$this->SetFont('Arial','',30);
+	
+	$this->Ln(22);
+    $this->Cell(0,10,$nombre_diploma,0,1,'C');
+	//$this->Ln(2);
+    //$this->Cell(0,10,$nombre_diploma2,0,1,'C');
+	
+	$lugar_fecha = "kugar fecha";
+	$numero_de_horas = "9 horas";
+	$this->SetFont('Arial','',15);
+	$this->Ln(3);
+	$this->Cell(0,5,$lugar_fecha,0,1,'C');
 
+	$this->SetFont('Arial','',15);
+	$this->Ln(3);
+    $this->Cell(0,5,$numero_de_horas,0,1,'C');
+	$this->SetFont('Arial','',14);
+	
+	
+	
+}
+public function footer()
+{    
+	$this->SetFont('Arial','',15);
+	 $this->SetY(-23);
+    $this->Cell(195,5,' ',0,0,'C');
+    $this->Cell(60,10,"Olga Castillejo",0,1,'C');
+
+}
 
 }
 
--- index.php
+++ index.php
@@ -1,19 +1,22 @@
 <?php
 require_once 'DiplomaPdf.php';
-require_once ("../config/db.php");//Contiene las variables de configuracion para conectar a la base de datos
-require_once ("../config/conexion.php");//Contiene funcion que conecta a la base de datos
+//require_once ("../config/db.php");//Contiene las variables de configuracion para conectar a la base de datos
+//require_once ("../config/conexion.php");//Contiene funcion que conecta a la base de datos
 
-$rut = $_GET['rut'];
+/*$rut = $_GET['rut'];
 $query=mysqli_query($con,"SELECT * from diploma where rut = $rut");
 	while($row=mysqli_fetch_array($query)){
 			$nombre =$row['nombre'];
 			$nombre.= " ".$row['apellido1'];
 			$nombre.= " ".$row['apellido2'];
-			}
-	
+			}*/
+$nombre = "Edwin Guasman Caceres";	
 $nombre_diploma = "curso de nuebvoas";
 $diplomaPdf = new DiplomaPdf('L','mm','letter');
-$diplomaPdf->renderFirma('firma_eugenio_azul.jpg',210,175,50);
+$diplomaPdf->SetAutoPageBreak(false);
+//$diplomaPdf->renderFirma('Firma_Liendo_0711.png',210,163,50);
+$diplomaPdf->renderFirma('Firma_Olga.png',220,150,20);
 $diplomaPdf->nombreCurso($nombre);
+$diplomaPdf->footer();
 $diplomaPdf->Output();
 ?>
