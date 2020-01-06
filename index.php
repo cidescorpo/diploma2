d716d145726f3a47a7a147ed505d90d2a690a44b
cidescorpo <51003050+cidescorpo@users.noreply.github.com>
2020-01-06 18:40:59 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -558,6 +558,19 @@ $firmarelator = "Firma_Parra.png";
 
 }
 
+else if ($curso ==  3996)
+{
+$nombrediploma1 = "Investigación de incidentes / Accidentes";
+$nombrediploma2 = "con Base en el Método ICAM";
+
+$lugarfecha = "Tierra Amarilla, 6 de enero de 2020";
+
+$horas = "8 horas";
+$nombrerelator = "Juan Ramón Flores";
+$firmarelator = "Firma_Flores.png";
+
+}
+
 $diplomaPdf = new DiplomaPdf('L','mm','letter');
 $diplomaPdf->SetAutoPageBreak(false);
 $diplomaPdf->renderFirma($firmarelator,210,163,50);
